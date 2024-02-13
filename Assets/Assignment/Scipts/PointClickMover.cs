cfa9527485e6edbcc5a3e14497fad00e2ff23fd8
dg015 <bruno.barbosa8@yahoo.com>
2024-02-13 17:17:00 -0500

    Health system working with UI and player cannot move after dead now

--- Assets/Assignment/Scipts/PointClickMover.cs
+++ Assets/Assignment/Scipts/PointClickMover.cs
@@ -2,29 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PointClickMover : MonoBehaviour
 {
     [SerializeField] private Vector3 Coordinates;
     [SerializeField] private Vector3 GoTo;
+    [SerializeField] public int health = 5;
+    [SerializeField] public bool dead = false;
+    public Slider slider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        slider.maxValue = health;
     }
 
+    public void BarHealth()
+    {
+        
+        slider.value = health;
+    }
+
+    private void die()
+    {
+        if (health <= 0)
+        { 
+            dead = true;    
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetMouseButton(0))
+        die();
+        BarHealth();
+        if (dead == false)
         {
-            GoTo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            GoTo.z = 0;
-            Debug.Log("yoo");
-            
+            if (Input.GetMouseButton(0))
+            {
+                GoTo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                GoTo.z = 0;
+                Debug.Log("yoo");
+
+            }
         }
-       
 
     }
 
