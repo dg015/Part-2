50910e8b560b42bcadd63340290b64dfa682ca68
dg015 <bruno.barbosa8@yahoo.com>
2024-02-13 15:57:02 -0500

    Player movement complete using lerp

--- Assets/Assignment/Scipts/PointClickMover.cs
+++ Assets/Assignment/Scipts/PointClickMover.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class PointClickMover : MonoBehaviour
 {
-    [SerializeField] private Vector2 Coordinates;
-    [SerializeField] private Vector2 GoTo;
+    [SerializeField] private Vector3 Coordinates;
+    [SerializeField] private Vector3 GoTo;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +16,22 @@ public class PointClickMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GoTo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetMouseButtonDown(0))
-        {
-            
-            
-            while (Coordinates != Vector2.Lerp(transform.position, GoTo, Time.deltaTime))
-            {
-                Coordinates = Vector2.Lerp(transform.position, GoTo, Time.deltaTime);
-            }
-            transform.position = Coordinates;
-        }
         
+        if (Input.GetMouseButton(0))
+        {
+            GoTo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GoTo.z = 0;
+            Debug.Log("yoo");
+            
+        }
+       
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        transform.position = Vector3.Lerp(transform.position, GoTo, Time.deltaTime * 2);
 
     }
 }
