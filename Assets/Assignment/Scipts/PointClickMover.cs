9c6b91a08ee991da9ee9416af361cc06bc2fed16
dg015 <bruno.barbosa8@yahoo.com>
2024-02-12 22:56:19 -0500

    Start of trying to make the player move with lerp

--- Assets/Assignment/Scipts/PointClickMover.cs
+++ Assets/Assignment/Scipts/PointClickMover.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PointClickMover : MonoBehaviour
 {
+    [SerializeField] private Vector2 Coordinates;
+    [SerializeField] private Vector2 GoTo;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class PointClickMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GoTo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            
+            while (Coordinates != Vector2.Lerp(transform.position, GoTo, Time.deltaTime))
+            {
+                Coordinates = Vector2.Lerp(transform.position, GoTo, Time.deltaTime);
+            }
+            transform.position = Coordinates;
+        }
         
+
     }
 }
