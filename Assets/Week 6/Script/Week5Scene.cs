505b85331a9ae15a6c3923bdb471836a9ebe7904
rodbruno <bruno.barbosa8@Yahoo.com>
2024-02-14 15:48:47 -0500

    Basic UI and next Scene button working

---
+++ Assets/Week 6/Script/Week5Scene.cs
@@ -0,0 +1,16 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Week5Scene : MonoBehaviour
+{
+    public void LoadWeek5Scene()
+    {
+
+        SceneManager.LoadScene(2);
+
+    }
+}
