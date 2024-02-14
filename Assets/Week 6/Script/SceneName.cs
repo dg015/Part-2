0d99610aae8aa881ee966fa21e435c9e6f272117
rodbruno <bruno.barbosa8@Yahoo.com>
2024-02-14 14:26:51 -0500

    Scenes now changing

---
+++ Assets/Week 6/Script/SceneName.cs
@@ -0,0 +1,26 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class SceneName : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneName = GetComponent<TextMeshProUGUI>();
+        sceneName.text = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
