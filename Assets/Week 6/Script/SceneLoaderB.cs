118beaa680801228ce62076f65092c93afd42e88
rodbruno <bruno.barbosa8@Yahoo.com>
2024-02-14 14:51:44 -0500

    Task 2 complete

---
+++ Assets/Week 6/Script/SceneLoaderB.cs
@@ -0,0 +1,24 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderB : MonoBehaviour
+{
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("yoo");
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+
+    }
+}
