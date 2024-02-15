e86f7597e638d0338a2928f32127c4bfccab7191
dg015 <bruno.barbosa8@yahoo.com>
2024-02-14 20:15:37 -0500

    Task 4a and 4b done Sorry for not making a branch for 4b separately

--- Assets/Week 6/Script/Week5Scene.cs
+++ Assets/Week 6/Script/Week5Scene.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Week5Scene : MonoBehaviour
 {
@@ -12,5 +13,17 @@ public class Week5Scene : MonoBehaviour
 
         SceneManager.LoadScene(2);
 
+    }
+
+    public void Ratio()
+    {
+        Debug.Log("yooooo");
+        Screen.SetResolution(1280,720,false);
+    }
+
+    public void FullHD()
+    {
+        Debug.Log("yooooo");
+        Screen.SetResolution(1920, 1080, false);
     }
 }
