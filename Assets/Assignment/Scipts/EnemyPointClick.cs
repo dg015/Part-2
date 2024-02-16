05d71ce1154b8fee5f1b260cd2292b9bed3730b7
dg015 <bruno.barbosa8@yahoo.com>
2024-02-16 12:07:14 -0500

    Enemy now follows  the player and looks directly at the player 4a( gets distance to get direction as well and translates this value to move towards player) 4b ( using the same vector it calculates the direction of the player and enemy to always look at player)

---
+++ Assets/Assignment/Scipts/EnemyPointClick.cs
@@ -0,0 +1,44 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPointClick : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float speed;
+    [SerializeField] private int health;
+    [SerializeField] private int deamage;
+    [SerializeField] private Vector2 distance;
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private float faceRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chase();
+    }
+
+
+    private void chase()
+    {
+        direction = player.position - transform.position;
+        if (direction.magnitude < 15)
+        {
+            faceRotation = Mathf.Atan2(direction.y, direction.x) *Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, faceRotation);
+            transform.Translate(distance * Time.deltaTime * speed);
+        
+
+        }
+
+
+    }
+
+}
