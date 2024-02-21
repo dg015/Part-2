ce858021ff4e5dbaaac1456a72858c4dd04e6184
Bruno Rodrigues Barbosa <rodbruno@shernet.sheridancollege.ca>
2024-02-21 14:15:04 -0500

    completed task 1

---
+++ Assets/Week 7/Scripts/PlayerSoccer.cs
@@ -0,0 +1,50 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+
+public class PlayerSoccer : MonoBehaviour
+{
+
+    [SerializeField] private Color col;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        col = spriteRenderer.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void PlayerSelected(bool selected)
+    {
+
+       if (selected)
+       {
+            spriteRenderer.color = Color.white;
+       }
+       else
+       {
+            spriteRenderer.color = col; 
+       }
+    }
+
+    private void OnMouseUp()
+    {
+        PlayerSelected(false);
+    }
+    private void OnMouseDown()
+    {
+
+        PlayerSelected(true);
+    }
+
+}
